cc14870cfa0121a5ed37db4d2e393c0a83bd88c7
Paul Frybarger <pfrybarger@gmail.com>
2012-08-03 17:14:30 -0500

    Changes to install script  - command line options  - 'user' and 'dev' installs  - more, will describe later

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,41 +1,107 @@
 #!/bin/sh
 
-# this attempts to install the ModelSEED and all dependencies
+# check for readlink, if not available warn user to use absolute paths
 
-# assume using current dir, ask later for install dir
+TB_LINK="https://github.com/ModelSEED/ModelSEED/tarball/master" # other branches?
+DEP_LINK="http://bioseed.mcs.anl.gov/~pfrybar/msdist"
 
-# check if mac os x, if no gcc/make then need to download xcode
+# check if running as root (sudo)
+ROOT=0
+if [ "$(id -u)" -eq "0" ]; then
+    ROOT=1
+fi
 
-# can either clone the repository, or download a tarball
-#git clone https://github.com/ModelSEED/Model-SEED-core.git ModelSEEDCore
+# parse the options (-t, -d, and -i)
+VERBOSE=0
+TYPE=user
+SET_INSTALL=0
+if [ $ROOT -eq 1 ]; then
+    LOCAL=/usr/local
+    INSTALL=/usr/local
+else
+    LOCAL=$HOME/local
+    INSTALL=$HOME/local
+fi
 
-# should this take options from: command line, script, or prompt?
-
-HOST=http://bioseed.mcs.anl.gov/~pfrybar/msdist
-GLOBAL=0             # option
-DIR=`pwd`/ModelSEED  # option
-LOCAL=$DIR/local     # option
-LOG=`pwd`/build.log  # option
-
-# todo: echo parameters into log file
-echo "Beginning ModelSEED installation" > $LOG
-
-# Check for required applications
-ERRORS=0
-for PROGRAM in perl cc make
+while getopts ":hvt:l:i:" optname
 do
-    if ! [ `which $PROGRAM` ];
-    then
-        ERRORS=1
-        echo "Could not find required program: '$PROGRAM'."
-    fi
+    case "$optname" in
+        "h")
+            # print usage
+            echo "usage: "
+            exit
+            ;;
+        "v")
+            # verbose
+            VERBOSE=1
+            ;;
+        "t")
+            # one of 'user' or 'dev'
+            if [ $OPTARG = "user" ]; then
+                TYPE=user
+            elif [ $OPTARG = "dev" ]; then
+                TYPE=dev
+            else
+                echo "Valid type (-t) options are 'user' and 'dev'."
+                exit
+            fi
+            ;;
+        "l")
+            LOCAL=`readlink -f $OPTARG`
+            ;;
+        "i")
+            INSTALL=`readlink -f $OPTARG`
+            SET_INSTALL=1
+            ;;
+        "?")
+            echo "Unknown option -$OPTARG"
+            # print usage
+            exit
+            ;;
+        ":")
+            echo "No value for option -$OPTARG"
+            # print usage
+            exit
+            ;;
+    esac
 done
 
-# stop after errors if we got them
-if [ $ERRORS -eq 1 ];
-then
-    echo "Please install program(s) listed above and try again."
-    exit
+if [ $TYPE = "user" ]; then
+    while true; do
+        echo "\nInstalling ModelSEED into '$LOCAL', do you want to continue? (y/n)"
+        read -p "" yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* )
+                echo "Cancelling, 'install.sh -h' for options"
+                exit
+                ;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+else
+    # set install directory to `pwd`/ModelSEED if not set
+    if [ $SET_INSTALL -eq 0 ]; then
+        INSTALL=`pwd`/ModelSEED
+    fi
+
+    while true; do
+        echo "\nInstalling ModelSEED into '$INSTALL', and dependencies into '$LOCAL', do you want to continue? (y/n)"
+        read -p "" yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* )
+                echo "Cancelling, 'install.sh -h' for options"
+                exit
+                ;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+fi
+
+OUT="/dev/null"
+if [ $VERBOSE -eq 1 ]; then
+    OUT="/dev/stdout"
 fi
 
 # setup download client (curl or wget)
@@ -47,172 +113,124 @@ then
     UA='wget -O'
 else
     echo "Please install either 'curl' or 'wget' and try again."
-exit
+    exit
 fi
 
-download_file(){
-    echo -n "Downloading file '$1' ..."
-    `$UA $1 $HOST/$1 1>>$LOG 2>&1`
-    echo " finished!"
+update_progress(){
+    PROGRESS=`expr $PROGRESS + 1`
+    echo -n "$PROGRESS" > .progress
 }
 
-check_error(){
-    if [ $? -ne 0 ];
-    then
-	echo "Error running install, check $LOG for details"
-	exit 2
-    fi
-}
+echo "Beginning ModelSEED installation..."
+echo "\tinstall type:" $TYPE > $OUT
 
-# download source code (todo: git or tarball)
-#if ! [ -f ModelSEED.tgz ];
-#then
-#    download_file ModelSEED.tgz
-#fi
-
-#tar -zxf ModelSEED.tgz
-
-cd ModelSEED
-mkdir local
-mkdir src
-
-# install cpanm if not found
-cd src
-if ! [ `which cpanm` ];
-then
-    download_file cpanm.pl
-    echo -n "Installing cpanm ..."
-    if [ $GLOBAL -eq 1 ];
-    then
-	sudo perl cpanm.pl App::cpanminus 1>>$LOG 2>&1; check_error
-    else
-	perl cpanm.pl -l $LOCAL App::cpanminus local::lib 1>>$LOG 2>&1; check_error
-	eval $(perl -I$LOCAL/lib/perl5 -Mlocal::lib=$LOCAL) 1>>$LOG 2>&1; check_error
-    fi
-    echo " finished!"
-fi
-
-# following packages need external libraries installed:
-#   - XML::Parser - expat
-#   - XML::LibXML - xml2 (which requires zlib, -lz)
-echo "main(){}" > test.c
-
-# `cc -lexpat test.c 1>/dev/null 2>&1`
-# if [ $? -ne 0 ];
-# then
-#     # need to install expat and XML::Parser manually
-#     download_file expat.tgz
-#     tar -xzf expat.tgz
-#     cd expat
-#     echo -n "Building expat ..."
-#     if [ $GLOBAL -eq 1 ];
-#     then
-# 	./configure 1>>$LOG 2>&1; check_error
-# 	make 1>>$LOG 2>&1; check_error
-# 	sudo make install 1>>$LOG 2>&1; check_error
-#     else
-# 	./configure --prefix=$LOCAL 1>>$LOG 2>&1; check_error
-# 	make 1>>$LOG 2>&1; check_error
-# 	make install 1>>$LOG 2>&1; check_error
-#     fi
-#     echo " finished!"
-#     cd ..
-
-#     download_file XML-Parser.tgz
-#     tar -xzf XML-Parser.tgz
-#     cd XML-Parser
-#     echo -n "Building XML::Parser ..."
-#     if [ $GLOBAL -eq 1 ];
-#     then
-# 	perl Makefile.PL 1>>$LOG 2>&1; check_error
-# 	make 1>>$LOG 2>&1; check_error
-# 	sudo make install 1>>$LOG 2>&1; check_error
-#     else
-# 	perl Makefile.PL EXPATLIBPATH=$LOCAL/lib EXPATINCPATH=$LOCAL/include INSTALL_BASE=$LOCAL 1>>$LOG 2>&1; check_error
-# 	make 1>>$LOG 2>&1; check_error
-# 	make install 1>>$LOG 2>&1; check_error
-#     fi
-#     echo " finished!"
-#     cd ..
-# fi
-
-`cc -lxml2 test.c 1>/dev/null 2>&1`
-if [ $? -ne 0 ];
-then
-    # need to install libxml2 and XML::LibXML manually
-    # first check if we need to install zlib too
-    `cc -lz test.c 1>/dev/null 2>&1`
-    if [ $? -ne 0 ];
-    then
-        # install zlib
-	download_file zlib.tgz
-	tar -xzf zlib.tgz
-	cd zlib
-	echo -n "Building zlib ..."
-	if [ $GLOBAL -eq 1 ];
-	then
-	    ./configure 1>>$LOG 2>&1; check_error
-	    make 1>>$LOG 2>&1; check_error
-	    sudo make install 1>>$LOG 2>&1; check_error
-	else
-	    ./configure --prefix=$LOCAL 1>>$LOG 2>&1; check_error
-	    make 1>>$LOG 2>&1; check_error
-	    make install 1>>$LOG 2>&1; check_error
-	fi
-	echo " finished!"
-	cd ..
-    fi
-
-    # install libxml2
-    download_file libxml2.tgz
-    tar -xzf libxml2.tgz
-    cd libxml2
-    echo -n "Building libxml2 (may take a few minutes) ..."
-    if [ $GLOBAL -eq 1 ];
-    then
-	./configure 1>>$LOG 2>&1; check_error
-	make 1>>$LOG 2>&1; check_error
-	sudo make install 1>>$LOG 2>&1; check_error
-    else
-	./configure --prefix=$LOCAL 1>>$LOG 2>&1; check_error
-	make 1>>$LOG 2>&1; check_error
-	make install 1>>$LOG 2>&1; check_error
-    fi
-    echo " finished!"
-    cd ..
-
-    # install XML::LibXML
-    download_file XML-LibXML.tgz
-    tar -xzf XML-LibXML.tgz
-    cd XML-LibXML
-    echo -n "Building XML::LibXML ..."
-    if [ $GLOBAL -eq 1 ];
-    then
-	perl Makefile.PL 1>>$LOG 2>&1; check_error
-	make 1>>$LOG 2>&1; check_error
-	sudo make install 1>>$LOG 2>&1; check_error
-    else
-	perl Makefile.PL XMLPREFIX=$LOCAL INSTALL_BASE=$LOCAL 1>>$LOG 2>&1; check_error
-	make 1>>$LOG 2>&1; check_error
-	make install 1>>$LOG 2>&1; check_error
-    fi
-    echo " finished!"
-    cd ..
-fi
-
-exit
-
-# now install carton and try to install dependencies
-cd ..
-    echo -n "Installing Perl dependencies (will take a few minutes) ..."
-if [ $GLOBAL -eq 1 ];
-then
-    sudo cpanm Carton 1>>$LOG 2>&1; check_error
-    sudo carton install 1>>$LOG 2>&1; check_error
+if [ $ROOT -eq 1 ]; then
+    echo "\troot install: yes" > $OUT
 else
-    cpanm -l $LOCAL Carton 1>>$LOG 2>&1; check_error
-    carton install 1>>$LOG 2>&1; check_error
+    echo "\troot install: no" > $OUT
 fi
 
-echo " finished!"
-echo "ModelSEED installation completed successfully!"
+echo "\tlocal dir:" $LOCAL > $OUT
+echo "\tinstall dir:" $INSTALL > $OUT
+
+if ! [ -d /tmp/MSInstall ]; then
+    mkdir /tmp/MSInstall
+fi
+
+cd /tmp/MSInstall
+
+if ! [ -f .progress ]; then
+    echo -n "0" > .progress
+fi
+
+PROGRESS=`cat .progress`
+
+# PROGRESS
+# 0 - install glpk (should allow cplex eventually)
+# 1 - install MFAToolkit
+# 2 - install MS manual dependencies (cpanm, zlib, libxml2, XML::LibXML)
+# 3 - install MS perl dependencies (via Build.PL)
+# 4 - install MS
+# 5 - run tests?
+
+if [ $PROGRESS -eq 0 ]; then
+    update_progress
+fi
+
+if [ $PROGRESS -eq 1 ]; then
+    update_progress
+fi
+
+if [ $PROGRESS -eq 2 ]; then
+    CPANM="cpanm"
+    if ! [ `which cpanm` ]; then
+        # download cpanm.pl
+        if ! [ -f cpanm.pl ]; then
+            $UA cpanm.pl $DEP_LINK/cpanm.pl 1>$OUT 2>&1
+            perl cpanm.pl -l $LOCAL App::cpanminus 1>$OUT 2>&1
+            CPANM=$LOCAL/bin/cpanm
+        fi
+    fi
+
+    echo "main(){}" > test.c
+    `cc -lxml2 test.c 1>/dev/null 2>&1`
+    if [ $? -ne 0 ]; then
+        # need to install libxml2 and XML::LibXML manually
+        # first check if we need to install zlib too
+        `cc -lz test.c 1>/dev/null 2>&1`
+        if [ $? -ne 0 ]; then
+            # install zlib
+            if ! [ -d zlib ]; then
+                $UA zlib.tgz $DEP_LINK/zlib.tgz 1>$OUT 2>&1
+                tar -xzvf zlib.tgz 1>$OUT 2>&1
+            fi
+
+            cd zlib
+            ./configure --prefix=$LOCAL 1>$OUT 2>&1
+            make 1>$OUT 2>&1
+            make install 1>$OUT 2>&1
+            cd ..
+        fi
+
+        # install libxml2
+        if ! [ -d libxml2 ]; then
+            $UA libxml2.tgz $DEP_LINK/libxml2.tgz 1>$OUT 2>&1
+            tar -xzvf libxml2.tgz 1>$OUT 2>&1
+        fi
+
+        cd libxml2
+        ./configure --prefix=$LOCAL 1>$OUT 2>&1
+        make 1>$OUT 2>&1
+        make install 1>$OUT 2>&1
+        cd ..
+
+        # install XML::LibXML
+        if ! [ -d XML-LibXML ]; then
+            $UA XML-LibXML.tgz $DEP_LINK/XML-LibXML.tgz 1>$OUT 2>&1
+            tar -xzvf XML-LibXML.tgz 1>$OUT 2>&1
+        fi
+
+        cd XML-LibXML
+        perl Makefile.PL XMLPREFIX=$LOCAL INSTALL_BASE=$LOCAL 1>$OUT 2>&1
+        make 1>$OUT 2>&1
+        make install 1>$OUT 2>&1
+        cd ..
+    fi
+
+    update_progress
+fi
+
+if [ $PROGRESS -eq 3 ]; then
+    # download ModelSEED tarball or git clone
+    if ! [ -d ModelSEED ]; then
+        $UA ModelSEED.tgz $TB_LINK 1>$OUT 2>&1
+        tar -xzvf ModelSEED.tgz 1>$OUT 2>&1
+        mv ModelSEED-* ModelSEED
+    fi
+
+    cd ModelSEED
+    perl Build.PL
+    ./Build installdeps --cpan_client 'cpanm' --install_base $LOCAL
+    ./Build install --install_base $LOCAL
+fi
+
