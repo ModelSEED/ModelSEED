8c488ffb581c370e9ff610252f58a322b7a8d298
Christopher Henry <chenry@mcs.anl.gov>
2012-09-25 15:54:24 -0500

    Added a command to manually reprint the FBAFiles for a biochemistry

---
+++ lib/ModelSEED/App/bio/Command/printdbfiles.pm
@@ -0,0 +1,29 @@
+package ModelSEED::App::bio::Command::printdbfiles;
+use strict;
+use common::sense;
+use base 'App::Cmd::Command';
+use Class::Autouse qw(
+    ModelSEED::Store
+    ModelSEED::Auth::Factory
+    ModelSEED::App::Helpers
+);
+sub abstract { return "Prints FBA database files used by the MFAToolkit" }
+sub usage_desc { return "bio printdbfiles [< biochemistry | biochemistry] [options]"; }
+sub opt_spec {
+    return (
+        ["help|h|?", "Print this usage information"],
+    );
+}
+
+sub execute {
+    my ($self, $opts, $args) = @_;
+    print($self->usage) && exit if $opts->{help};
+    my $auth  = ModelSEED::Auth::Factory->new->from_config;
+    my $store = ModelSEED::Store->new(auth => $auth);
+    my $helper = ModelSEED::App::Helpers->new();
+    my ($biochemistry,my $ref) = $helper->get_object("biochemistry", $args, $store);
+    $self->usage_error("Must specify an biochemistry to use") unless(defined($biochemistry));
+    $biochemistry->printDBFiles({forceprint => 1});
+}
+
+1;
