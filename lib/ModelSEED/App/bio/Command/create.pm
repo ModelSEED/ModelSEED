d46039fdcc9ee3ff72566503bd9ab33359ba5bda
seaver <samseaver@gmail.com>
2013-02-04 11:00:20 -0600

    Added permament UUIDs to match current MS Biochem for compartments

--- lib/ModelSEED/App/bio/Command/create.pm
+++ lib/ModelSEED/App/bio/Command/create.pm
@@ -40,19 +40,19 @@ sub execute {
     $new_biochemistry->addCompoundFromHash({id=>['cpd00067'],names=>['H+','Proton'],namespace=>'ModelSEED',formula=>['H'],charge=>[1]});
 
     #add compartments
-    $new_biochemistry->addCompartmentFromHash({id=>'e',name=>'Extracellular',hierarchy=>0});
-    $new_biochemistry->addCompartmentFromHash({id=>'w',name=>'Cell Wall',hierarchy=>1});
-    $new_biochemistry->addCompartmentFromHash({id=>'p',name=>'Periplasm',hierarchy=>2});
-    $new_biochemistry->addCompartmentFromHash({id=>'c',name=>'Cytosol',hierarchy=>3});
-    $new_biochemistry->addCompartmentFromHash({id=>'g',name=>'Golgi',hierarchy=>4});
-    $new_biochemistry->addCompartmentFromHash({id=>'r',name=>'Endoplasmic Reticulum',hierarchy=>4});
-    $new_biochemistry->addCompartmentFromHash({id=>'l',name=>'Lysosome',hierarchy=>4});
-    $new_biochemistry->addCompartmentFromHash({id=>'n',name=>'Nucleus',hierarchy=>4});
-    $new_biochemistry->addCompartmentFromHash({id=>'h',name=>'Chloroplast',hierarchy=>4});
-    $new_biochemistry->addCompartmentFromHash({id=>'m',name=>'Mitochondria',hierarchy=>4});
-    $new_biochemistry->addCompartmentFromHash({id=>'x',name=>'Peroxisome',hierarchy=>4});
-    $new_biochemistry->addCompartmentFromHash({id=>'v',name=>'Vacuole',hierarchy=>4});
-    $new_biochemistry->addCompartmentFromHash({id=>'d',name=>'Plastid',hierarchy=>4});
+    $new_biochemistry->addCompartmentFromHash({id=>'e',name=>'Extracellular',hierarchy=>0,uuid=>"0A33AC94-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'w',name=>'Cell Wall',hierarchy=>1,uuid=>"0A33BA9A-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'p',name=>'Periplasm',hierarchy=>2,uuid=>"0A33C2CE-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'c',name=>'Cytosol',hierarchy=>3,uuid=>"0A33CBC0-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'g',name=>'Golgi',hierarchy=>4,uuid=>"0A33D494-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'r',name=>'Endoplasmic Reticulum',hierarchy=>4,uuid=>"0A33DD4A-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'l',name=>'Lysosome',hierarchy=>4,uuid=>"0A33E632-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'n',name=>'Nucleus',hierarchy=>4,uuid=>"0A33EEA2-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'h',name=>'Chloroplast',hierarchy=>4,uuid=>"0A33F776-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'m',name=>'Mitochondria',hierarchy=>4,uuid=>"0A34007C-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'x',name=>'Peroxisome',hierarchy=>4,uuid=>"0A3409A0-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'v',name=>'Vacuole',hierarchy=>4,uuid=>"0A34135A-D74E-11E1-8F32-85923D9902C7"});
+    $new_biochemistry->addCompartmentFromHash({id=>'d',name=>'Plastid',hierarchy=>4,uuid=>"0A341C38-D74E-11E1-8F32-85923D9902C7"});
 
     my $ref = $helper->process_ref_string($args->[0], "biochemistry", $auth->username);
     verbose("Creating biochemistry with name ".$ref."\n");
