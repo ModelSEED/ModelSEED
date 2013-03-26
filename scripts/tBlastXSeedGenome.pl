8c9ec5f00f2c99eb09d32655752334a1a61e534a
Christopher Henry <chenry@mcs.anl.gov>
2013-03-25 21:51:47 -0500

    Committing changes to tblastx script

--- scripts/tBlastXSeedGenome.pl
+++ scripts/tBlastXSeedGenome.pl
@@ -4,61 +4,42 @@ use strict;
 use SeedUtils;
 use File::Path;
 use ModelSEED::Client::SAP;
-
 my $genome = $ARGV[0];
 my $directory = $ARGV[1];
 my $queryfile = $ARGV[2];
+my $output = $ARGV[3];
 my $sap = ModelSEED::Client::SAP->new();
-my $genomeHash = $sap->genome_contigs({
-	-ids => [$genome]
-});
-my $query = [];
+File::Path::mkpath $directory."/temp/";
+my ($in, $filename) = File::Temp::tempfile("XXXXXXX",DIR => $directory."/temp/");
 open (ONE, "<", $queryfile);
 while (my $Line = <ONE>) {
-	$Line =~ s/\r//;
-	chomp($Line);
-	push(@{$query},[split(/\t/,$Line)]);
+	my $queryLine = [split(/\t/,$Line)];
+	my $fastaString = SeedUtils::create_fasta_record($queryLine->[0],undef, $queryLine->[1]);
+	print $in $fastaString;
 }
 close(ONE);
-my $results;
-if (defined($genomeHash->{$genome})) {
-	my $contigHash = $sap->contig_sequences({
-		-ids => $genomeHash->{$genome}
+close($in);
+my $genomes = [$genome];
+if ($genome eq "ALL") {
+	my $genomeHash = $sap->all_genomes({
+		-complete => 1,
+		-prokaryotic => 1
 	});
-	File::Path::mkpath $directory."/".$genome."/";
-	open ( TWO, ">", $directory."/".$genome."/fasta");
-	foreach my $contig (keys(%{$contigHash})) {
-		my $fastaString = SeedUtils::create_fasta_record($contig,0,$contigHash->{$contig});
-		print TWO $fastaString;
+	foreach my $genome (keys(%{$genomeHash})) {
+		push(@{$genomes},$genome);
 	}
-	close(TWO);
-	system("/vol/rast-bcr/2010-1124/linux-rhel5-x86_64/bin/formatdb -i ".$directory."/".$genome."/fasta -p F"); 
-	open(THREE, ">".$directory."/".$genome."/query");
-	#for (my $i=0; $i < 1; $i++) {
-	for (my $i=0; $i < @{$query}; $i++) {
-		my $fastaString = SeedUtils::create_fasta_record($query->[$i]->[0],undef, $query->[$i]->[1]);
-		print THREE $fastaString;
-	}
-	close(THREE);
-	system("/vol/rast-bcr/2010-1124/linux-rhel5-x86_64/bin/blastall -i ".$directory."/".$genome."/query -d ".$directory."/".$genome."/fasta -p tblastx -FF -e 1.0e-5 -m 8 -o ".$directory."/".$genome."/query.out");
-#	my $output = [];
-#	open (FOUR, "<", $directory."/".$genome."/query.out");
-#	while (my $Line = <FOUR>) {
-#		$Line =~ s/\r//;
-#		chomp($Line);
-#		my $lineArray = [split(/\t/,$Line)];
-#		if (defined($lineArray->[11])) {
-#			$results->{$query->[$i]->[0]}->{$lineArray->[1]} = {
-#				identity => $lineArray->[2],
-#				"length" => $lineArray->[3],
-#				qstart => $lineArray->[6],
-#				qend => $lineArray->[7],
-#				tstart => $lineArray->[8],
-#				tend => $lineArray->[9],
-#				evalue => $lineArray->[10],
-#				bitscore => $lineArray->[11],
-#			}
-#		}
-#	}
-#	close(FOUR);
 }
+open (FOUR, "<", $output);
+print FOUR "Query\tContig\tIdentity\tLength\tMismatchCount\tGapCount\tQStart\tQEnd\tTStart\tTEnd\tEValue\tBitScore\n";
+for (my $i=0; $i < @{$genomes}; $i++) {
+	my ($out, $fileout) = File::Temp::tempfile("XXXXXXX",DIR => $directory."/temp/");
+	close($out);
+	system("/vol/rast-bcr/2010-1124/linux-rhel5-x86_64/bin/blastall -i ".$filename." -d ".$directory."/".$genomes->[$i]."/fasta -p tblastn -FF -e 1.0e-3 -m 8 -o ".$fileout);
+	open (FOUR, "<", $fileout);
+	while (my $Line = <FOUR>) {
+		$Line =~ s/\r//;
+		chomp($Line);
+		print $Line."\n";
+	}
+}
+close(FOUR);
