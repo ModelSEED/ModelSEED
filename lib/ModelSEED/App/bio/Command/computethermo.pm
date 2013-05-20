7011fc94d4d50badb3e8b5969f34821019162efc
seaver <samseaver@gmail.com>
2013-05-20 13:13:39 -0500

    Refactored App::bio::Command::computethermo

--- lib/ModelSEED/App/bio/Command/computethermo.pm
+++ lib/ModelSEED/App/bio/Command/computethermo.pm
@@ -1,46 +1,33 @@
 package ModelSEED::App::bio::Command::computethermo;
 use strict;
 use common::sense;
-use base 'App::Cmd::Command';
+use ModelSEED::App::bio;
+use base 'ModelSEED::App::BioBaseCommand';
 use ModelSEED::utilities;
-use Class::Autouse qw(
-    ModelSEED::Store
-    ModelSEED::Auth::Factory
-    ModelSEED::App::Helpers
-);
 sub abstract { return "Balance all the reactions in a biochemistry"; }
 sub usage_desc { return "bio balancerxns [name]"; }
 sub opt_spec {
     return (
-    	["verbose|v", "Print comments on command actions"],
 	["direction|d", "Update directionality to match computed thermoReversibility (default value: 1)",]
    	);
 }
 
-sub execute {
-    my ($self, $opts, $args) = @_;
+sub sub_execute {
+    my ($self, $opts, $args, $bio) = @_;
 
     ModelSEED::utilities::set_verbose(1) if $opts->{verbose};
     
-    my $auth  = ModelSEED::Auth::Factory->new->from_config;
-    my $store = ModelSEED::Store->new(auth => $auth);
-    my $helper = ModelSEED::App::Helpers->new();
-    $self->usage_error("Must specify a name for the object to be created") unless(defined($args->[0]));
-    my ($biochemistry,$ref) = $helper->get_object("biochemistry", $args, $store);
-    $self->usage_error("Biochemistry ".$args->[0]." not found") unless defined($biochemistry);
-    
     my $args={direction=>1};
-
     if(defined($opts->{direction})){
 	$args->{direction}=$opts->{direction};
     }
 
-    foreach my $rxn (@{$biochemistry->reactions()}){
+    foreach my $rxn (@{$bio->reactions()}){
 	$rxn->checkReactionCueBalance();
 	$rxn->calculateEnergyofReaction();
 	$rxn->estimateThermoReversibility($args);
     }
-    $store->save_object($ref,$biochemistry);
+    $self->save_bio($bio);
 }
 
 1;
