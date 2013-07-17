a0ce12e0203e81f17c8a1e8b5288da4393e1181d
Matt DeJongh <dejongh@hope.edu>
2013-07-17 13:10:22 -0400

    Changes to import iJR904 with biomass equation

--- lib/ModelSEED/MS/Factories/FBAMODELFactory.pm
+++ lib/ModelSEED/MS/Factories/FBAMODELFactory.pm
@@ -117,18 +117,23 @@ sub createModel {
         my $id = $rxn->{DATABASE}->[0];
         if ( $id =~ m/bio\d+/ ) {
 
-	    # SO: change scientific notation to decimal
-	    $rxn->{EQUATION}->[0] =~ s/5e-05/.00005/g;
-	    $rxn->{EQUATION}->[0] =~ s/6e-06/.000006/g;
-	    $rxn->{EQUATION}->[0] =~ s/6\.9e-05/.000069/g;
-	    $rxn->{EQUATION}->[0] =~ s/3e-06/.000003/g;
-	    # SO: get rid of acyl-glycerophosphoethanolamine and acyl-glycerophosphoglycerol
-	    # from the biomass because they are products of phospholipases and couldn't get
-	    # them to grow on BU0_AF
-	    $rxn->{EQUATION}->[0] =~ s/0\.010152 cpd15647 \+ //g;
-	    $rxn->{EQUATION}->[0] =~ s/0\.000421 cpd15648 \+ //g;
-	    # keep peptidoglycan inside the cell
-	    $rxn->{EQUATION}->[0] =~ s/\[e\]//g;
+	    if ($args->{id} eq 'iSO783') {
+		# SO: change scientific notation to decimal
+		$rxn->{EQUATION}->[0] =~ s/5e-05/.00005/g;
+		$rxn->{EQUATION}->[0] =~ s/6e-06/.000006/g;
+		$rxn->{EQUATION}->[0] =~ s/6\.9e-05/.000069/g;
+		$rxn->{EQUATION}->[0] =~ s/3e-06/.000003/g;
+		# SO: get rid of acyl-glycerophosphoethanolamine and acyl-glycerophosphoglycerol
+		# from the biomass because they are products of phospholipases and couldn't get
+		# them to grow on BU0_AF
+		$rxn->{EQUATION}->[0] =~ s/0\.010152 cpd15647 \+ //g;
+		$rxn->{EQUATION}->[0] =~ s/0\.000421 cpd15648 \+ //g;
+		# keep peptidoglycan inside the cell
+		$rxn->{EQUATION}->[0] =~ s/\[e\]//g;
+	    }
+	    elsif ($args->{id} eq 'iJR904') {
+		$rxn->{EQUATION}->[0] = "(0.050000) cpd00345 + (0.000050) cpd00022 + (0.488000) cpd00035 + (0.001000) cpd00018 + (0.281000) cpd00051 + (0.229000) cpd00132 + (0.229000) cpd00041 + (0.000129) cpd15650 + (0.000006) cpd00010 + (0.126000) cpd00052 + (0.087000) cpd00084 + (0.024700) cpd00115 + (0.025400) cpd00356 + (0.025400) cpd00241 + (0.024700) cpd00357 + (0.000010) cpd00015 + (0.250000) cpd00053 + (0.250000) cpd00023 + (0.582000) cpd00033 + (0.154000) cpd00155 + (0.203000) cpd00038 + (0.090000) cpd00119 + (0.276000) cpd00322 + (0.428000) cpd00107 + (0.008400) cpd15652 + (0.326000) cpd00039 + (0.146000) cpd00060 + (0.002150) cpd00003 + (0.000050) cpd00004 + (0.000130) cpd00006 + (0.000400) cpd00005 + (0.001935) cpd15653 + (0.027600) cpd15654 + (0.000464) cpd15655 + (0.176000) cpd00066 + (0.210000) cpd00129 + (0.000052) cpd15657 + (0.035000) cpd00118 + (0.205000) cpd00054 + (0.007000) cpd00264 + (0.000003) cpd00078 + (0.241000) cpd00161 + (0.054000) cpd00065 + (0.131000) cpd00069 + (0.003000) cpd00026 + (0.136000) cpd00062 + (0.402000) cpd00156 => (45.5608) cpd00008 + (45.56035) cpd00067 + (45.5628) cpd00009 + (0.7302) cpd00012";
+	    }
 
             # Add as a biomass equation
 	    my $bioobj = $model->add("biomasses", ModelSEED::MS::Biomass->new({
@@ -159,7 +164,7 @@ sub createModel {
             }
 	    my @pegs;
 	    # SO: pegs are separated by AND and OR; for now just collect all the pegs
-	    if ($args->{id} eq 'iSO783') {
+	    if ($args->{id} eq 'iSO783' || $args->{id} eq 'iJR904') {
 		foreach my $peg (@{$rxn->{PEGS}}) {
 		    while ($peg =~ /(peg\.\d+)/g) {
 			push @pegs, "fig|".$genome_id.".".$1;
